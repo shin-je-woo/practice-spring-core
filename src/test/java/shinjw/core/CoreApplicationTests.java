8624dcc1412c132cc7d97afb8b7ae5c65b422653
shin.jewoo <shinjw0926@naver.com>
2023-02-18 20:43:26 +0900

    create project

---
+++ src/test/java/shinjw/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package shinjw.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
