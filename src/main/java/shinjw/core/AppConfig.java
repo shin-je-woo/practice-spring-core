61a07f6899bec4cb9d7211faeb2f8c4cae54c5ee
shin.jewoo <shinjw0926@naver.com>
2023-02-19 14:41:36 +0900

    AppCoifg 리펙터링 - 역할 분리

--- src/main/java/shinjw/core/AppConfig.java
+++ src/main/java/shinjw/core/AppConfig.java
@@ -1,7 +1,10 @@
 package shinjw.core;
 
 
+import shinjw.core.discount.DiscountPolicy;
+import shinjw.core.discount.FixDiscountPolicy;
 import shinjw.core.discount.RateDiscountPolicy;
+import shinjw.core.member.MemberRepository;
 import shinjw.core.member.MemberService;
 import shinjw.core.member.MemberServiceImpl;
 import shinjw.core.member.MemoryMemberRepository;
@@ -11,10 +14,19 @@ import shinjw.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new RateDiscountPolicy();
+        return new FixDiscountPolicy();
     }
 }
